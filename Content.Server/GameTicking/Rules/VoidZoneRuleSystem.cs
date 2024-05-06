2e66a8e07d94f1bebf46815182c6a039107e6510
Dexler <69513582+DexlerXD@users.noreply.github.com>
2024-05-06 20:59:31 +0300

    add code copyright

--- Content.Server/GameTicking/Rules/VoidZoneRuleSystem.cs
+++ Content.Server/GameTicking/Rules/VoidZoneRuleSystem.cs
@@ -1,3 +1,5 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+
 /*
 Созревает вопрос, как я всё это писал с нулевыми знаниями C#. Ответ я в душе не ебу
 Jokerge
